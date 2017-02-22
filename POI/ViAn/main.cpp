d8eedd04e79ce90d5f89a40edfde69c6e240e3b6
henpe071 <henper098@gmail.com>
2017-02-22 14:44:29 +0100

    A first version of the GUI
    A very simple version of the GUI with a window, a menu, some button. All
    without functionality.

--- POI/ViAn/main.cpp
+++ POI/ViAn/main.cpp
@@ -1,6 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+/**
+ * @brief qMain
+ * Constructor
+ * @param argc an int variable
+ * @param argv a list of chars
+ * @return exits the program
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
