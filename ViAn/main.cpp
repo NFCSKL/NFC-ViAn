79a30d2cbd245a8184dd5b5da46fc03018fe7e0a
Fleskvegg <patrik.lundgren@outlook.com>
2017-03-03 16:26:01 +0100

    overwrite main with masters main

--- ViAn/main.cpp
+++ ViAn/main.cpp
@@ -1,10 +1,6 @@
-#include "Video/video_player.h"
-#include "Test/test_video_player.h"
-#include "GUI/mainwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
-#include <QTest>
-#include "Filehandler/filehandler.h"
-#include "Test/filehandlertest.h"
+
 /**
  * @brief qMain
  * Constructor
@@ -17,13 +13,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-
-    test_video_player* video_player_test = new test_video_player();
-    filehandlertest* file_handler_test = new filehandlertest();
-
-    QTest::qExec(video_player_test) ;
-    QTest::qExec(file_handler_test) ;
 
     return a.exec();
 }
