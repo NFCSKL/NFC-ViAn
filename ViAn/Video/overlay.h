1ecb4274a8b2427fe42db98c65333d003459a820
Parso <parso619student.liu.se>
2017-03-10 09:38:54 +0100

    added two tests for the overlay, and getters in overlay class

--- ViAn/Video/overlay.h
+++ ViAn/Video/overlay.h
@@ -15,8 +15,10 @@ public:
     void set_showing_overlay(bool value);
     void toggle_overlay();
     void draw_overlay(QImage &img);
-    void set_overlay_tool(SHAPES shape);
-    void set_overlay_colour(QColor colour);
+    void set_tool(SHAPES shape);
+    void set_colour(QColor colour);
+    QColor get_colour();
+    SHAPES get_shape();
     void mouse_pressed(QPoint pos);
     void mouse_released(QPoint pos);
     void mouse_moved(QPoint pos);
