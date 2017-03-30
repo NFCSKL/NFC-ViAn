107d0f38dcde074ac3e170070b19ce9ab135f672
Parso <parso619student.liu.se>
2017-03-30 17:12:18 +0200

    Added draw for area

--- ViAn/Video/shapes/analysarea.cpp
+++ ViAn/Video/shapes/analysarea.cpp
@@ -1,7 +1,6 @@
 #include "analysarea.h"
 
 AnalysArea::AnalysArea() {
-
 }
 
 /**
@@ -11,10 +10,13 @@ AnalysArea::AnalysArea() {
  * @return Returns the frame with drawing.
  */
 cv::Mat AnalysArea::draw(cv::Mat &frame) {
-
+    for (std::pair<cv::Point, cv::Point> line : lines) {
+        cv::line(frame, line.first, line.second, colour, LINE_THICKNESS);
+    }
     return frame;
 }
 
 void AnalysArea::add_point(QPoint pos) {
+    points.push_back(Shape::qpoint_to_point(pos));
 }
 
