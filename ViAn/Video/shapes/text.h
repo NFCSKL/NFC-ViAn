574b69e4419afd48a11fff26c7b2e252a7df234b
Parso <parso619student.liu.se>
2017-03-31 14:57:46 +0200

    Misspelling.

--- ViAn/Video/shapes/text.h
+++ ViAn/Video/shapes/text.h
@@ -9,7 +9,7 @@ public:
     cv::Mat draw(cv::Mat &frame) override;
     void handle_new_pos(QPoint pos) override;
 
-    // Constants describing the limit and presicion of the font scale value.
+    // Constants describing the limit and precision of the font scale value.
     static constexpr double FONT_SCALE_MIN = 0.5, FONT_SCALE_MAX = 5.0, FONT_SCALE_STEP = 0.1;
     static constexpr int FONT_SCALE_DECIMALS = 1;
 private:
