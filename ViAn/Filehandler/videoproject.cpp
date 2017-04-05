d679bca429758ab6c75c7eb348037c42174bc9db
Fleskvegg <patrik.lundgren@outlook.com>
2017-04-05 20:03:53 +0200

    New videoProject class with some members and constructor

--- ViAn/Filehandler/videoproject.cpp
+++ ViAn/Filehandler/videoproject.cpp
@@ -1,5 +1,8 @@
 #include "videoproject.h"
 
-VideoProject::VideoProject()
-{
+VideoProject::VideoProject(Video* v){
+    this->video = v;
+}
+Video* VideoProject::get_video(){
+    return this->video;
 }
