8035c758440d6b790b5e296191b0b389c1c5dcab
Fredrik <fredrikiselius@users.noreply.github.com>
2018-04-24 13:46:25 +0200

    Revert "Frame num save"

--- ViAn/GUI/mainwindow.cpp
+++ ViAn/GUI/mainwindow.cpp
@@ -143,7 +143,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
     connect(project_wgt, &ProjectWidget::marked_video, video_wgt, &VideoWidget::clear_tag);
     connect(project_wgt, &ProjectWidget::marked_video, video_wgt->frame_wgt, &FrameWidget::set_video_project);
     connect(project_wgt, &ProjectWidget::marked_video, drawing_wgt, &DrawingWidget::set_video_project);
-    connect(project_wgt, &ProjectWidget::update_brightness_contrast, video_wgt, &VideoWidget::update_brightness_contrast);
 
     connect(project_wgt, &ProjectWidget::project_closed, drawing_wgt, &DrawingWidget::clear_overlay);
     connect(project_wgt, &ProjectWidget::project_closed, video_wgt, &VideoWidget::clear_current_video);
@@ -574,11 +573,9 @@ void MainWindow::gen_report() {
  * Creates a dialog for choosing contrast and brightness values.
  */
 void MainWindow::cont_bri() {
-    if (!video_wgt->get_current_video_project()) return;
     emit set_status_bar("Opening contrast/brightness settings");
     ManipulatorDialog* man_dialog = new ManipulatorDialog(video_wgt->get_brightness(), video_wgt->get_contrast(), this);
     connect(man_dialog, SIGNAL(values(int,double)), video_wgt, SLOT(update_brightness_contrast(int,double)));
-    connect(man_dialog, SIGNAL(values(int,double)), video_wgt, SLOT(set_brightness_contrast(int,double)));
     man_dialog->exec();
 }
 
--- ViAn/Project/video.h
+++ ViAn/Project/video.h
@@ -7,11 +7,10 @@
 #include <QString>
 #include "Filehandler/saveable.h"
 struct VideoState{
-    int frame = -1;
-    double contrast = 1;
+    int frame = 0;
+    int contrast = 0;
     int brightness = 0;
     int rotation = 0;
-
     VideoState(){}
     VideoState(VideoState&rh){
         frame = rh.frame;
