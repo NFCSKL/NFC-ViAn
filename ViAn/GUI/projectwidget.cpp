fa63662b142ad63956fa67a73da149bbb8709e4f
Fredrik Iselius <fredrik.iselius@gmail.com>
2018-06-30 13:48:40 +0200

    The drawing widget is now reloaded when a sequence item is clicked. Drawing tool is also deselected

--- ViAn/GUI/projectwidget.cpp
+++ ViAn/GUI/projectwidget.cpp
@@ -623,6 +623,7 @@ void ProjectWidget::tree_item_clicked(QTreeWidgetItem* item, const int& col) {
         auto seq_item = dynamic_cast<SequenceItem*>(item);
         VideoItem* vid_item = dynamic_cast<VideoItem*>(item->parent());
         vid_item->get_video_project()->get_video()->state.frame = seq_item->get_index();
+        emit set_video_project(vid_item->get_video_project());
         emit marked_video_state(vid_item->get_video_project(),
                                 vid_item->get_video_project()->get_video()->state);
 
